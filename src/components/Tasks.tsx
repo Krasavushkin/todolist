9d4b9127919291d9f607b55babc86fe3c90046c2
krasavushkin <artemkrasavushkin@gmail.com>
2023-10-26 22:22:57 +0300

    additional task

---
+++ src/components/Tasks.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {type} from "os";
+
+type TasksArr = {
+    taskId: number;
+    title: string;
+    isDone: boolean
+}
+
+type StudentsArray = {
+
+
+}
+
+
+type TasksType = {
+    title: string;
+    tasks: TasksArr[];
+    students: StudentsArray
+}
+
+export const Tasks = (props: TasksType) => {
+    debugger
+    return (
+        <ul>
+            <h1> {props.title}</h1>
+            {props.tasks.map((task) => {
+                return(
+                <li key={task.taskId}><input type={"checkbox"} checked={task.isDone}/>
+            <span> {task.title}</span></li>
+                )})
+            };
+            <ul>
+                {props.students.map((student) => {
+                    return (
+                        <li key={student}> {student} </li>
+                    )})
+                }
+            </ul>
+
+        </ul>
+    )
+};
+
+
+/*    ;
+};*/
+
